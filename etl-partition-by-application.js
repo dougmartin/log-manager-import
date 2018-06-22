cbe5c25a27833b55c8fd5485c11aedbaedd2727c
Doug Martin <doug@zoopdoop.com>
2018-06-22 14:17:52 -0400

    Added parallel uploads to etl-partition-by-application

--- etl-partition-by-application.js
+++ etl-partition-by-application.js
@@ -1,69 +1,59 @@
 const AWS = require('aws-sdk');
 const zlib = require('zlib');
 const async = require('async');
+const s3 = new AWS.S3();
+
+const NUM_PARALLEL_TRANSFORMS = 10;
+const NUM_PARALLEL_UPLOADS = 10;
 
 AWS.config.loadFromPath('./aws-config.json');
 
-const s3 = new AWS.S3();
-
-const listObjects = ({continuationToken, perObjectCallback}) => {
+const listObjects = ({startAfter, perObjectCallback}) => {
   const params = {
     Bucket: 'log-manager-data',
     Prefix: 'processed-logs/',
     MaxKeys: 1000
   };
-  if (continuationToken) {
-    params.ContinuationToken = continuationToken;
+  if (startAfter) {
+    params.StartAfter = startAfter;
   }
   s3.listObjectsV2(params, (err, data) => {
     if (err) {
       console.error(err);
     }
     else {
-      const next = () => {
-        process.nextTick(() => {
-          drainContents();
-        });
-      };
-      const drainContents = () => {
-        if (data.Contents.length > 0) {
-          const object = data.Contents.shift();
-          const params = {
-            Bucket: 'log-manager-data',
-            Key: object.Key
-          };
-          s3.getObject(params, (err, data) => {
+      const lastKey = data.Contents.length > 0 ? data.Contents[data.Contents.length - 1].Key : null;
+      async.eachLimit(data.Contents, NUM_PARALLEL_TRANSFORMS, (object, done) => {
+        const params = {
+          Bucket: 'log-manager-data',
+          Key: object.Key
+        };
+        s3.getObject(params, (err, data) => {
+          if (err) {
+            return callback(err);
+          }
+          zlib.gunzip(data.Body, (err, body) => {
             if (err) {
-              console.error(err);
-              next();
+              return callback(err);
             }
-            else {
-              zlib.gunzip(data.Body, (err, body) => {
-                if (err) {
-                  console.log(err);
-                  next();
-                }
-                else {
-                  perObjectCallback({object, body, done: next});
-                }
-              });
-            }
+            perObjectCallback({object, body, done});
+          });
+        });
+      }, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        else if (lastKey) {
+          process.nextTick(() => {
+            listObjects({startAfter: lastKey, perObjectCallback});
           });
         }
-        else {
-          if (data.NextContinuationToken) {
-            process.nextTick(() => {
-              listObjects({continuationToken: data.NextContinuationToken, perObjectCallback});
-            });
-          }
-        }
-      };
-      drainContents();
+      });
     }
   });
 };
 
-const listLogEntries = ({body, perLogEntryCallback}) => {
+const forEachLogEntry = ({body, perLogEntryCallback}) => {
   body.toString().split('\n').forEach((line) => {
     if (line.length > 0) {
       perLogEntryCallback(JSON.parse(line));
@@ -74,31 +64,24 @@ const listLogEntries = ({body, perLogEntryCallback}) => {
 listObjects({perObjectCallback: ({object, body, done}) => {
   const applications = {};
   console.log(object.Key);
-  listLogEntries({body, perLogEntryCallback: (entry) => {
+  forEachLogEntry({body, perLogEntryCallback: (entry) => {
     applications[entry.application] = applications[entry.application] || [];
     applications[entry.application].push(JSON.stringify(entry));
   }});
-  Object.keys(applications).forEach((application) => {
-    zlib.gzip(applications[application].join('\n'), (err, gzipped) => {
+
+  async.eachOfLimit(applications, NUM_PARALLEL_UPLOADS, (lines, application, callback) => {
+    zlib.gzip(lines.join('\n'), (err, gzipped) => {
       if (err) {
-        console.log(err);
-        done();
+        return callback(err);
       }
-      else {
-        const applicationKey = object.Key.replace(/^processed-logs\//, `per-application-logs/${application.replace(/\//g, '').trim()}/`);
-        s3.upload({
-          Bucket: 'log-manager-data',
-          Key: applicationKey,
-          Body: gzipped,
-          ContentType: 'application/json',
-          ContentEncoding: 'gzip',
-        }, (err) => {
-          if (err) {
-            console.log(err);
-          }
-          done();
-        });
-      }
+      const applicationKey = object.Key.replace(/^processed-logs\//, `per-application-logs/${application.replace(/\//g, '').trim()}/`);
+      s3.upload({
+        Bucket: 'log-manager-data',
+        Key: applicationKey,
+        Body: gzipped,
+        ContentType: 'application/json',
+        ContentEncoding: 'gzip',
+      }, callback);
     });
-  });
+  }, done);
 }});
